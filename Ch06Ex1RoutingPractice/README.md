008d6e30311273fa4e846b256c596a0b2b4c3e83
7248510 <calebkre19@gmail.com>
2021-02-28 16:33:04 -0500

    Routing practice

---
+++ Ch06Ex1RoutingPractice/README.md
@@ -0,0 +1,43 @@
+# Chapter 6 Routing Practices
+# Application running:
+![Application running](Images/applicationRunning.PNG)
+
+# Index
+![Index](Images/index.PNG)
+
+# Home
+![Home](Images/home.PNG)
+
+# Privacy
+![Privacy](Images/privacy.PNG)
+
+# liftOff
+![liftoff](Images/liftOff.PNG)
+
+# Countdown without value
+![Countdown without value](Images/countDown_noval.PNG)
+
+# Countdown with value
+![Countdown with value](Images/countDown_val.PNG)
+
+# Countdown with routing
+![Countdown with routing](Images/countDown_Routing.PNG)
+
+# Display
+![display](Images/display.PNG)
+
+# Display/abc123
+![display 123](Images/display_abc123.PNG)
+
+# missing second segment
+![missingSegment](Images/missingsecondSegment.PNG)
+
+# Segment value 5
+![segment 5](Images/secondSegment5.PNG)
+
+# Segment not changed
+![segment not changed](Images/segmentnotchanged.PNG)
+
+# Segment change id to num
+![segment id to num](Images/segmentchangeidtonum.PNG)
+
---
+++ Ch06Ex1RoutingPractice/RoutingPractice/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Home");
+        }
+
+        public IActionResult Privacy()
+        {
+            return Content("Privacy");
+        }
+
+        public IActionResult Display(string id)
+        {
+            if (id == null)
+            {
+                return Content("No ID supplied.");
+            }
+            else
+            {
+                return Content("ID: " + id);
+            }
+        }
+        [Route("[action]/{start}/{end?}/{message?}")] //Segment name must match the parameter
+        public IActionResult Countdown(int start, int end = 0, string message = "")
+        //public IActionResult Countdown(int id= 0) //without routing
+        {
+            string contentString = "Counting down:\n";
+            for (int i = start; i >= end; i--) //i starts at start and has to be less than the end 
+            {
+                contentString += i + "\n";
+            }
+            contentString += message; //Appends the message value
+            return Content(contentString);
+        }
+    }
+}
