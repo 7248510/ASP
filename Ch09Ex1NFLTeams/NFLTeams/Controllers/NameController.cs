755e45849487bccc51ac587c87e717758dd1d996
7248510 <calebkre19@gmail.com>
2021-03-18 23:39:40 -0400

    Chapter 9

---
+++ Ch09Ex1NFLTeams/NFLTeams/Controllers/NameController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NFLTeams.Models; //Import to fix the session error
+
+namespace NFLTeams.Controllers
+{
+    public class NameController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            var session = new NFLSession(HttpContext.Session);
+            var model = new TeamListViewModel
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv(),
+                Teams = session.GetMyTeams(),
+                UserName = session.GetName() //Exactly like the favorites controller. The only difference is adding the username variable
+            };
+
+            return View(model);
+        }
+        [HttpPost]
+        public RedirectToActionResult Change(TeamListViewModel model)
+        {
+            var session = new NFLSession(HttpContext.Session);
+            session.SetName(model.UserName);
+            return RedirectToAction("Index", "Home", new
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv()
+            });
+        }
+    }
+}
---
+++ Ch09Ex1NFLTeams/readme.md
@@ -0,0 +1,30 @@
+# Chapter 8 data transfer from controllers
+# Application running:
+![Application running](Images/applicationRunning.PNG)
+
+# Favorites Page
+![Product list default](Images/Favorites.PNG)
+
+# Add Favorites:
+![Product list default](Images/addFavorites.PNG)
+
+# Add Favorites Message:
+![Product list admin default](Images/addFavoritesMessage.PNG)
+
+# Favorites clear
+![Product list admin modified](Images/favoritesCleared.PNG)
+
+# Favorites Before Change
+![Added Prompt](Images/favoritesPageBeforeChange.PNG)
+
+# My Favorites
+![Added product](Images/myFavorite.PNG)
+
+# Change Name(Name Index)
+![Updated Prompt](Images/nameIndex.PNG)
+
+# Change Name John
+![Updated Product](Images/changeNameJohn.PNG)
+
+# Changed name to John
+![Updated Product](Images/johnsFavorites.PNG)
