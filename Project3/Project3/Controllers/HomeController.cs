8025635109f13654aab69edc8de0ccf6799a2ebf
7248510 <calebkre19@gmail.com>
2021-04-24 17:48:51 -0400

    Project 3

---
+++ Project3/Project3/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project3.Models;
+using Project3.Controllers;
+
+namespace Project3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            ViewBag.discountPercent = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(quotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.calculateSales();
+                ViewBag.discountPercent = model.discountamount(ViewBag.FV);
+            }
+            else
+            {
+                ViewBag.FV = 0;
+                ViewBag.discountPercent = 0;
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ Project3/Project3/Models/quotationModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project3.Models
+{
+    public class quotationModel
+    {
+        [Required(ErrorMessage = "Please enter a sale price.")]
+        [Range(0, int.MaxValue, ErrorMessage = "The sales price is not greater than zero.")]
+        public decimal? subTotal { get; set; }
+        //End Sub total
+        [Required(ErrorMessage = "Please enter a discount percent.")]
+        [Range(0, 100.0, ErrorMessage = "The percent rate needs to be between 0 and 100%")]
+        public decimal? discountPercent { get; set; }
+        public decimal? calculateSales()
+        {
+            decimal? discountAmount = subTotal * (discountPercent / 100);
+            return discountAmount;
+        }
+        public decimal? discountamount(decimal? passMe)
+        {
+            decimal? calc = subTotal - passMe;
+            decimal? total = calc; //-subTotal 
+            return total;
+        }
+    }
+}
